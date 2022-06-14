4a6f2a480bb6355671c336f5cdd88e779daf3aaf
onyedikacollins10 <67547815+onyedikacollins10@users.noreply.github.com>
2022-06-14 22:34:27 +0100

    Add files via upload
    todos list app

---
+++ my todos list/scripts.js
@@ -0,0 +1,27 @@
+let input = prompt('what would you like to do?');
+const todos = ['coding every day', 'eating every day', 'biscuit', 'mango'];
+while ( input !== 'quit' && input !== 'q') {
+
+    if (input === 'list') {
+        console.log('***********')
+        for (let i = 0; i < todos.length; i++){
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log('***********')
+    } else if (input === 'new') {
+        const newTodo = prompt ('ok, what is the new todo?');
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list!`)
+    } else if (input === 'delete') {
+        const index = parseInt(prompt ('ok, enter an index to delete'));
+        if (!Number.isNaN(index)){
+            const deleted = todos.splice(index, 1)
+            console.log(`ok, deleted ${deleted[0]}`);
+        } else {
+            console.log('unknown index')
+        }
+        
+    }
+    input = prompt('what would you like to do?')
+}
+console.log('ok quit the app!')
